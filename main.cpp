12ce6754b829402509b8caba2a9e03c54c12bdb5
Kazoo <84203761+KAZOO323@users.noreply.github.com>
2025-04-17 02:59:36 -0400

    Data file loading implemented
    OnlineGDB (the compiler used to grade our work) does not recognize CSV files. At all. Not sure what they expect me to do here, but for the moment I converted the data to a .txt file and moved on with properly implementing data reading. The method I wrote should also work with a csv file, theoretically. Theoretically.

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 #include "airport.h"
 #include <string>
 
-// Loads a given CSV file into a given graph object
-void loadCSV(const std::string& filename, Graph& g) {
+// Loads a given text file into a given graph object. OnlineGDB does not recognize .csv files, so this will have to do.
+void loadTXT(const std::string& filename, Graph& g) {
     // Open file
     std::ifstream file(filename);
     // Ensure file is actually open
@@ -23,17 +23,27 @@ void loadCSV(const std::string& filename, Graph& g) {
     while (getline(file, line)) {
         std::stringstream ss(line);
         std::string origin, dest, originCity, destCity, distStr, costStr;
-
+        
+        // Gather variable data
         getline(ss, origin, ',');
         getline(ss, dest, ',');
-        getline(ss, originCity, ',');
-        getline(ss, destCity, ',');
+        
+        // Handle city strings properly
+        if (ss.peek() == '"') ss.ignore();
+        getline(ss, originCity, '"');
+        ss.ignore(1, ',');
+        
+        if (ss.peek() == '"') ss.ignore();
+        getline(ss, destCity, '"');
+        ss.ignore(1, ',');
+
         getline(ss, distStr, ',');
         getline(ss, costStr, ',');
-
+        
         int distance = std::stoi(distStr);
         int cost = std::stoi(costStr);
-
+        
+        // Add airports and flights
         g.addAirport(origin);
         g.addAirport(dest);
         g.addFlight(origin, dest, distance, cost);
@@ -47,7 +57,7 @@ void loadCSV(const std::string& filename, Graph& g) {
 int main() {
     Graph g;
 
-    loadCSV("airports.csv", g);
+    loadTXT("airports.txt", g);
 
     g.printGraph();
 
